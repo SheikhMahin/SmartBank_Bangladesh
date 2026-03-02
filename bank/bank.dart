da7e3d5797aa4986817699ea98f5d426a19db021
SheikhMahin <mahinrahman268@gmail.com>
2026-03-02 15:20:40 +0600

    Implement transfer and showAllBalances methods in Bank class

--- bank/bank.dart
+++ bank/bank.dart
@@ -16,4 +16,32 @@ class Bank {
     }
     return null;
   }
+
+  void transfer(
+    String fromAccountNumber,
+    String toAccountNumber,
+    double amount,
+  ) {
+    Account? sender = findAccount(fromAccountNumber);
+    Account? receiver = findAccount(toAccountNumber);
+
+    if (sender != null &&
+        receiver != null &&
+        sender.getBalance() - amount >= 500) {
+      sender.withdraw(amount);
+      receiver.deposit(amount);
+      print(
+        "Transferred $amount Tk from ${sender.accountHolderName} to ${receiver.accountHolderName}",
+      );
+    } else {
+      print("One or both accounts not found for transfer.");
+    }
+  }
+
+  void showAllBalances() {
+    print("All Account Balances:");
+    for (var account in _accounts) {
+      print("${account.accountHolderName}: ${account.getBalance()} Tk");
+    }
+  }
 }
